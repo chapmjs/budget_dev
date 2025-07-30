c3fae3eeccdf67e7c99f0e8127f5171deadcdc89
John Chapman <john.chapman@systemsresearch.linkpc.net>
2025-07-30 17:06:10 -0600

    going  back to original location of files. keeping python app

---
+++ 20250703_dev_notes.R
@@ -0,0 +1,4 @@
+# 2025 07 30
+# was able to get a basic python budget tracking tool designed and deployed to:
+# https://nvsid.shinyapps.io/jnk_budget_tracker/
+# I was not able to include data persistence through google drive
